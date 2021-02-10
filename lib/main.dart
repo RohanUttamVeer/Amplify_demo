c0433d39ade91652434bc0ee410da66b609eeb12
RohanUttamVeer <rohanveer04@gmail.com>
2021-02-10 14:33:58 +0530

    Amplify Demo App

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:amplify_demo/verification_page.dart';
+import 'package:flutter/material.dart';
+
+import 'auth_service.dart';
+import 'camera_flow.dart';
+import 'login_page.dart';
+import 'sign_up_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// 1
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    _authService.showLogin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Gallery App',
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+
+      // 1
+      home: StreamBuilder<AuthState>(
+          // 2
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            // 3
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  // 4
+                  // Show Login Page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(
+                        builder: (context) => LoginPage(
+                            didProvideCredentials:
+                                _authService.loginWithCredentials,
+                            shouldShowSignUp: _authService.showSignUp)),
+                  // Show Verification Code Page
+                  if (snapshot.data.authFlowStatus ==
+                      AuthFlowStatus.verification)
+                    MaterialPage(
+                        builder: (context) => VerificationPage(
+                            didProvideVerificationCode:
+                                _authService.verifyCode)),
+
+// Show Camera Flow
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.session)
+                    MaterialPage(
+                        builder: (context) =>
+                            CameraFlow(shouldLogOut: _authService.logOut)),
+
+                  // 5
+                  // Show Sign Up Page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.signUp)
+                    MaterialPage(
+                        builder: (context) => SignUpPage(
+                            didProvideCredentials:
+                                _authService.signUpWithCredentials,
+                            shouldShowLogin: _authService.showLogin))
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              // 6
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+
+      // home: Navigator(
+      //     pages: [
+      //     MaterialPage(builder:(context) => LoginPage()),
+      //   MaterialPage(builder:(context) => SignUpPage())],
+      //
+      //             onPopPage: (route, result) => route.didPop(result),
+    );
+  }
+}
